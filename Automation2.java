560c7ec535552852f10940e10e718bf39d05dfb8
divya vandra <divyavandra@gmail.com>
2023-02-16 22:34:18 +0000

    hello

---
+++ Automation2.java
@@ -0,0 +1,7 @@
+package PracticeB;
+
+public class Automation2 {
+    public static void main(String[] args) {
+        System.out.println("How are you all");
+    }
+}
---
+++ Manual2.java
@@ -0,0 +1,8 @@
+package PracticeB;
+
+public class Manual2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello All");
+    }
+}
