9803a2fe0ed8aa0b5c15f868bc4d8089a801affb
divya vandra <divyavandra@gmail.com>
2023-02-16 22:12:40 +0000

    first session

---
+++ AutomationB.java
@@ -0,0 +1,9 @@
+package PracticeB;
+
+public class AutomationB {
+    public static void main(String[] args) {
+
+        System.out.println("Practice Session 2");
+
+    }
+}
---
+++ ManualB.java
@@ -0,0 +1,11 @@
+package PracticeB;
+
+public class ManualB {
+    public static void main(String[] args) {
+        System.out.println("Manual Session");
+
+        // This is a group B practice session
+    }
+
+
+}
